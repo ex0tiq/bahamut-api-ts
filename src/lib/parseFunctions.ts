2164ce63ca98bf34477fa8077ced31c0593fe7bd
Christian Holzwarth <christian@holzwarth.family>
2022-11-16 23:59:39 +0100

    Add parse functions

---
+++ src/lib/parseFunctions.ts
@@ -0,0 +1,28 @@
+/**
+ * Parse a string to a boolean value
+ * @param {string} string
+ * @returns {null|boolean}
+ */
+const parseBool = (string: string) => {
+    if ((string.toLowerCase() === "true") || (string.toLowerCase() === "1")) {
+        return true;
+    } else if ((string.toLowerCase() === "false") || (string.toLowerCase() === "0")) {
+        return false;
+    } else {
+        return null;
+    }
+};
+
+const hexToRGB = (hex: string, alpha: string) => {
+    const r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+};
+
+export { parseBool, hexToRGB };
