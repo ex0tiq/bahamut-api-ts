f6eb1ef19c590cf64df03b41e9098e6db9a236dd
Christian Holzwarth <christian@holzwarth.family>
2022-11-19 02:28:47 +0100

    Copy from bot

--- src/modules/GlobalSchedulers.ts
+++ src/modules/GlobalSchedulers.ts
@@ -3,6 +3,7 @@ import scheduler, { Job } from "node-schedule";
 import logger from "./Logger";
 import { getLatestLodestoneNews } from "../lib/getFunctions";
 import BahamutClient from "bahamutbot/src/modules/BahamutClient";
+import { setLatestLodestoneNews } from "../lib/setFunctions";
 
 export default class GlobalSchedulers {
     private _apiHandler;
@@ -19,14 +20,15 @@ export default class GlobalSchedulers {
                 logger.log("Running lodestone news scheduler.");
 
                 const latestLodestonePosts = await getLatestLodestoneNews(this._apiHandler.manager);
+                if (!latestLodestonePosts) return;
 
-                this._apiHandler.manager.broadcastHandler.broadcastToAll((_client: BahamutClient, obj: any) => {
-                    // TODO Implement additional check for enabled lodestone notification categories
+
+                await this._apiHandler.manager.broadcastHandler.broadcastToAll(async (_client: BahamutClient, obj: any) => {
+                    const { getGuildSettings } = require(obj.rootPath + "/lib/getFunctions");
 
                     // Flatten array and sort by date
-                    const { flattenArray } = require("../lib/toolFunctions");
                     const { DateTime } = require("luxon");
-                    let arr = flattenArray(obj.latestPosts);
+                    let arr = obj.latestPosts.flat();
 
                     if (arr.length <= 0) return;
 
@@ -34,14 +36,41 @@ export default class GlobalSchedulers {
                         return DateTime.fromISO(a2.time) - DateTime.fromISO(a1.time);
                     });
 
-                    for (const e of arr) {
-                        // Post all entries
+                    for (const [, guild] of _client.guilds.cache) {
+                        // TODO Implement additional check for enabled lodestone notification categories
+                        // filter for type on message object
+                        const sendMessages = obj.latestPosts;
+
+                        const guild_settings = await getGuildSettings(_client, guild);
+
+                        // Skip of guild has no lofestone news channel set
+                        if (!guild_settings.ffxiv_lodestone_news_channel || !guild.channels.cache.has(guild_settings.ffxiv_lodestone_news_channel)) continue;
+                        const lodestone_channel = guild.channels.cache.get(guild_settings.ffxiv_lodestone_news_channel);
+
+                        if (!lodestone_channel) return null;
+
+                        try {
+                            for (const m of obj.sendMessages) {
+                                // @ts-ignore
+                                await lodestone_channel.send(m);
+                            }
+                        } catch (ex) {
+                            console.error("Unable to post to lodestone channel:", ex);
+                        }
 
                     }
 
                 }, false, { latestPosts: latestLodestonePosts }, false);
 
-
+                // set last lodestone ids
+                await setLatestLodestoneNews(this._apiHandler.manager,
+                    latestLodestonePosts.topics[latestLodestonePosts.topics.length - 1],
+                    latestLodestonePosts.notices[latestLodestonePosts.notices.length - 1],
+                    latestLodestonePosts.maintenance[latestLodestonePosts.maintenance.length - 1],
+                    latestLodestonePosts.updates[latestLodestonePosts.updates.length - 1],
+                    latestLodestonePosts.status[latestLodestonePosts.status.length - 1],
+                    latestLodestonePosts.developers[latestLodestonePosts.developers.length - 1]
+                );
             } catch (ex) {
                 console.error(ex);
             }
@@ -51,6 +80,7 @@ export default class GlobalSchedulers {
     public get scheduler() {
         return this._scheduler;
     }
+
     public get schedules() {
         return this._schedules;
     }
