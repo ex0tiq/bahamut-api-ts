ff1fff816f099f3fd29a2d0f8d17a70ddf98670d
Christian Holzwarth <christian@holzwarth.family>
2022-11-20 02:16:16 +0100

    Add fashion report scheduler task

--- src/modules/GlobalSchedulers.ts
+++ src/modules/GlobalSchedulers.ts
@@ -4,6 +4,9 @@ import { getLatestLodestoneNews } from "../lib/getFunctions";
 import BahamutClient from "bahamutbot/src/modules/BahamutClient";
 import { setLatestLodestoneNews } from "../lib/setFunctions";
 import { BahamutAPIHandler } from "../index";
+import axios from "axios";
+import { DateTime } from "luxon";
+import { GlobalGuildSettings } from "../../typings";
 
 export default class GlobalSchedulers {
     private _apiHandler;
@@ -15,6 +18,7 @@ export default class GlobalSchedulers {
     constructor(apiHandler: BahamutAPIHandler) {
         this._apiHandler = apiHandler;
 
+        // Lodestone task
         this._scheduler.scheduleJob("*/10 * * * *", async () => {
             try {
                 logger.log("Running lodestone news scheduler.");
@@ -26,6 +30,7 @@ export default class GlobalSchedulers {
                     const { getGuildSettings } = require(obj.rootPath + "/lib/getFunctions");
 
                     // Flatten array and sort by date
+                    // eslint-disable-next-line no-shadow
                     const { DateTime } = require("luxon");
                     let arr: any[] = (Object.values(obj.latestPosts)).flat();
 
@@ -77,6 +82,71 @@ export default class GlobalSchedulers {
                     latestLodestonePosts.status[latestLodestonePosts.status.length - 1]?.id || undefined,
                     latestLodestonePosts.developers[latestLodestonePosts.developers.length - 1]?.id || undefined
                 );
+            } catch (ex) {
+                console.error(ex);
+            }
+        });
+
+        // Reddit fashion report task
+        this._scheduler.scheduleJob("*/2 * * * *", async () => {
+            try {
+                logger.log("Running fashion report scheduler.");
+
+                const settings: GlobalGuildSettings | null = <GlobalGuildSettings | null>await this._apiHandler.dbHandler.guildSettings.getDBGuildSettings("global");
+                if (!settings) return null;
+
+                let res = await axios.request({
+                    url: "https://reddit.com/r/ffxiv/search.json",
+                    params: {
+                        "q": "author:kaiyoko title:Fashion Report - Full Details",
+                        "sort": "new",
+                        "limit": 3,
+                    },
+                });
+
+                if (res && res.data) res = res.data;
+                else return;
+
+                if (res && res.data && res.data.children) res = res.data.children.map((e: { data: any; }) => e.data);
+                else return;
+
+                // Abort if no post found
+                if (!res || !Array.isArray(res) || res.length <= 0) return;
+                // Abort if post title does not match fashion report schema
+                if (!res[0].title.toLowerCase().match(/.*Fashion Report - Full Details.*/gi)) return;
+                // Abort current check if no new fashion report found
+                if ((res[0] && res[0].id) && (settings && settings.lastFashionReport && settings.lastFashionReport === res[0].id)) return;
+
+                const date = res[0].title.match(/\d{1,2}\/\d{1,2}\/\d{1,4}/);
+
+                if (Array.isArray(date) && date.length > 0 && (settings && settings.lastFashionReportDate && settings.lastFashionReportDate === date[0])) return;
+                if (settings && settings.lastFashionReportDate && (DateTime.fromFormat(date[0], "M/d/yyyy") <= DateTime.fromFormat(settings.lastFashionReportDate, "M/d/yyyy"))) return;
+
+                await this._apiHandler.broadcastHandler.broadcastToAll(async (_client: BahamutClient, obj: any) => {
+                    for (const [, guild] of _client.guilds.cache) {
+                        const { getGuildSettings } = require(obj.rootPath + "/lib/getFunctions");
+
+                        const guild_settings = await getGuildSettings(_client, guild),
+                            { EmbedBuilder } = require("discord.js");
+
+                        if (!guild_settings.ffxiv_fashion_report_channel || !guild.channels.cache.has(guild_settings.ffxiv_fashion_report_channel)) continue;
+                        const fashionreport_channel = guild.channels.cache.get(guild_settings.ffxiv_fashion_report_channel);
+
+                        if (obj.post) {
+                            // @ts-ignore
+                            await fashionreport_channel!.send({ embeds: [(new EmbedBuilder()
+                                        .setTitle(obj.post.title)
+                                        .setColor(_client.bahamut.config.primary_message_color)
+                                        .setImage(obj.post.url)
+                                        .setURL("https://reddit.com" + obj.post.permalink)
+                                )] });
+                        }
+                    }
+                }, false, { post: res[0] });
+
+                // Update last fashion report
+                await this._apiHandler.dbHandler.guildSettings.setDBGuildSetting("global", "lastFashionReport", res[0].id, "string");
+                if (Array.isArray(date) && date.length > 0) await this._apiHandler.dbHandler.guildSettings.setDBGuildSetting("global", "lastFashionReportDate", date[0], "string");
             } catch (ex) {
                 console.error(ex);
             }
